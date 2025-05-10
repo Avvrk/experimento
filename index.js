44afcd33d363990528193fd85b648d058cabe231
Avvrk <jomaldonado480@gmail.com>
2025-05-10 13:21:20 -0500

    z

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 const express = require("express");
 const axios = require("axios");
-const cron = require("node-cron");
-require('dotenv').config(); 
+require("dotenv").config();
 
 const app = express();
-const BACKEND_A_URL = 'https://backend-a.example.com/ping';
 
-app.get('/ping', (req, res) => {
-  console.log('📥 Backend B recibió ping');
-  res.send('Pong from B');
-});
-
-// Cron job: cada 5 minutos y 10 segundos (no exacto para evitar colisión)
-cron.schedule('*/5 * * * *', async () => {
+app.get("/ping", (req, res) => {
   setTimeout(async () => {
-    try {
-      const res = await axios.get("https://botd-e34u.onrender.com/bot/ping")
-      console.log('✅ Backend B envió ping a Backend A:', res.data);
-    } catch (err) {
-      console.error('❌ Error en Backend B al pingear A:', err.message);
-    }
-  }, 10000); // espera 10 segundos antes de hacer el ping
+    await axios.get("https://botd-e34u.onrender.com/ping");
+  }, 5 * 60 * 1000);
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`🚀 Backend B corriendo en ${process.env.PORT}`);
+  console.log(`Puerto: ${process.env.PORT}`);
 });
